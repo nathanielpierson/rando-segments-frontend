8f5afeec9d7e3898502394cbb4db23365f4be982
nathanielpierson <nathanielepierson@gmail.com>
2025-05-17 18:44:41 -0400

    add fastest level times to map function

--- src/LevelsIndex.jsx
+++ src/LevelsIndex.jsx
@@ -1,7 +1,20 @@
-export function LevelsIndex() {
+export function LevelsIndex( {levels} ) {
+  console.log(levels);
+  console.log("in LevelsIndex")
   return (
     <div>
-      h
+      <h1>All Levels ({levels.length} total)</h1>
+      {levels.map((level) => (
+        <div key={level.id}>
+          <h3>{level.name}</h3>
+          <img src={level.image_url || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuX96Zx4GsEJeHhzhhs1jEG6XPuCF4LoxugA&s"}
+          style={{ height: "300px", width: "420px" }}
+          className="hover:opacity-70 transition-opacity duration-300"
+          alt={level.name} />
+          {/* ternary operators are on this to add 0s if seconds or milliseconds are single digit */}
+          <p>fastest time: {level.fastest_time_min}:{level.fastest_time_sec < 10 ? "0" + level.fastest_time_sec : level.fastest_time_sec}.{level.fastest_time_milli}</p>
+          </div>
+      ))}
     </div>
   )
 }
--- src/LevelsPage.jsx
+++ src/LevelsPage.jsx
@@ -1,12 +1,22 @@
-import { LevelsIndex } from './LevelsIndex'
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { LevelsIndex } from './LevelsIndex';
 
 export function LevelsPage() {
+  const [levels, setLevels] = useState([]);
+  const handleIndex = () => {
+  axios.get("http://localhost:3000/levels.json").then((response) => {
+    setLevels(response.data);
+    console.log(response.data);
+  })
+}
+useEffect(handleIndex, []);
   return (
     <div>
       <p>
         LevelsPage
       </p>
-        <LevelsIndex />
+        <LevelsIndex levels={levels}/>
     </div>
   )
 }
