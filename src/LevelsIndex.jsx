40951410370b005b6f15f25abfd001a3a6bd3a8f
nathanielpierson <nathanielepierson@gmail.com>
2025-05-20 19:53:49 -0400

    change streamer list for streamers more active currently

--- src/LevelsIndex.jsx
+++ src/LevelsIndex.jsx
@@ -1,9 +1,18 @@
 export function LevelsIndex( {levels} ) {
   console.log(levels);
   console.log("in LevelsIndex")
+
+  h
+  
   return (
     <div>
       <h1>All Levels ({levels.length} total)</h1>
+      <button
+      onClick={handleClick}
+      className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition"
+    >
+      Update All
+    </button>
       {levels.map((level) => (
         <div key={level.id}>
           <h3>{level.name}</h3>
@@ -12,7 +21,7 @@ export function LevelsIndex( {levels} ) {
           className="hover:opacity-70 transition-opacity duration-300"
           alt={level.name} />
           {/* ternary operators are on this to add 0s if seconds or milliseconds are single digit */}
-          <p>fastest time: {level.fastest_time_min}:{level.fastest_time_sec < 10 ? "0" + level.fastest_time_sec : level.fastest_time_sec}.{level.fastest_time_milli}</p>
+          <p>fastest time: {level.fastest_time_min}:{level.fastest_time_sec < 10 ? "0" + level.fastest_time_sec : level.fastest_time_sec}.{level.fastest_time_milli < 10 ? "0" + level.fastest_time_milli : level.fastest_time_milli}</p>
           </div>
       ))}
     </div>
